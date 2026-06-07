5b590344f825116ce103e0fc94a34995e46e782a
paulnarag <naragpaulandrew@gmail.com>
2026-06-07 19:43:34 +0800

    created a generatePassword function, code is from PasswordLab.io

--- script.js
+++ script.js
@@ -9,6 +9,29 @@ if (!(window.crypto && window.crypto.getRandomValues)) {
     console.log("Crypto API is unavailable")
 }
 
+function generatePassword(length, includeUppercase, includeNumbers, includeSymbols) {
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
+    const numberChars = "0123456789"
+    const specialChars = "!@#$%^&*()-=_+[]{}|;:,.<>?/"
+
+    let allChars = ""
+    let password = ""
+
+    if (includeNumbers) allChars += numberChars
+    if (includeUppercase) allChars+=uppercaseChars
+    if (!includeUppercase) allChars+=lowercaseChars
+
+    const allCharsLength = allChars.length
+
+    for (let i=0; i<length; i++) {
+        const randomIndex = Math.floor(window.crypto.getRandomValues(new Uint32Array(1))[0] / (0xFFFFFFFF + 1) * allCharsLength)
+        password += allChars.charAt(randomIndex)
+    }
+
+    return password
+}
+
 lengthOutput.innerHTML = lengthSlider.value
 
 lengthSlider.oninput = function() {
