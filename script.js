e3a21fbfbafab84c45190cf8307c657b123b526b
paulnarag <naragpaulandrew@gmail.com>
2026-06-07 19:31:54 +0800

    check for Crypto API

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const passwordGenerator = document.getElementById("passwordGenerator")
 const lengthSlider = document.getElementById("lengthSlider")
 const lengthOutput = document.getElementById("lengthOutput")
 
+if (!(window.crypto && window.crypto.getRandomValues)) {
+    console.log("Crypto API is unavailable")
+}
+
 lengthOutput.innerHTML = lengthSlider.value
 
 lengthSlider.oninput = function() {
